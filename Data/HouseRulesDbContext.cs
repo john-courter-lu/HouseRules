d5e5bb1884ea74bb2d171aff915290ba504bb700
John Lu <johnlu2006@gmail.com>
2023-10-03 14:43:12 -0500

    update Data/Dbcontext.cs

---
+++ Data/HouseRulesDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using HouseRules.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HouseRules.Data;
+public class HouseRulesDbContext : IdentityDbContext<IdentityUser>
+{
+    private readonly IConfiguration _configuration;
+
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    public HouseRulesDbContext(DbContextOptions<HouseRulesDbContext> context, IConfiguration config) : base(context)
+    {
+        _configuration = config;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserProfile>().HasData(new UserProfile
+        {
+            Id = 1,
+            IdentityUserId = "dbc40bc6-0829-4ac5-a3ed-180f5e916a5f",
+            FirstName = "Admina",
+            LastName = "Strator",
+            Address = "101 Main Street",
+        });
+
+    }
+}
