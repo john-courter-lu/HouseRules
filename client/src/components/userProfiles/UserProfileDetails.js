6815b9d91aa8f12a434f2ea51c51ea8a483278d8
John Lu <johnlu2006@gmail.com>
2023-10-05 15:18:20 -0500

    create UserProfileDetails UI component

--- client/src/components/userProfiles/UserProfileDetails.js
+++ client/src/components/userProfiles/UserProfileDetails.js
@@ -1,1 +1,91 @@
-export const UserProfileDetails = () => {}
+import { useEffect, useState } from "react"
+import { getUserById } from "../../managers/userProfileManager"
+import { useParams } from "react-router-dom"
+import { Table } from "reactstrap"
+
+
+export const UserProfileDetails = () => {
+    const [userProfile, setUserProfile] = useState(null)
+
+    const { userId } = useParams();
+
+    useEffect(() => {
+        getUserById(userId).then(setUserProfile)
+    }, [])
+
+    if (!userProfile) {
+        return null;
+    }
+
+    return (
+        <div className="container">
+            <h2 className="sub-menu">{userProfile.firstName} {userProfile.lastName} Details</h2>
+            <Table>
+                <tbody>
+                    <tr>
+                        <th scope="row">First Name</th>
+                        <td>{userProfile.firstName}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Last name</th>
+                        <td>{userProfile.lastName}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Address</th>
+                        <td>{userProfile.address}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Email</th>
+                        <td>{userProfile.identityUser.email}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">User Name</th>
+                        <td>{userProfile.identityUser.userName}</td>
+                    </tr>
+                </tbody>
+            </Table>
+            <h5 className="sub-menu">Assiged Chores</h5>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Difficulty</th>
+                        <th>Frequency</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userProfile.choreAssignments.map((ca) => (
+                        <tr key={`choreAssignment--${ca.id}`}>
+                            <td>{ca.chore.name}</td>
+                            <td>{ca.chore.difficulty}</td>
+                            <td>{ca.chore.choreFrequencyDays} Day(s)</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            <h5 className="sub-menu">Completed Chores</h5>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Difficulty</th>
+                        <th>Frequency</th>
+                        <th>Date Completed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userProfile.choreCompletions.map((cc) => (
+                        <tr key={`choreCompletion--${cc.id}`}>
+                            <td>{cc.chore.name}</td>
+                            <td>{cc.chore.difficulty}</td>
+                            <td>{cc.chore.choreFrequencyDays} Day(s)</td>
+                            <td>{new Date(cc.completedOn).toLocaleDateString('en-US')}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+        </div>
+    )
+
+}
