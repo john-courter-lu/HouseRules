ab7cd9a8a84a2b0f7fa5fd92324a038409457742
John Lu <johnlu2006@gmail.com>
2023-10-05 14:20:52 -0500

    data access functions for UI components

---
+++ client/src/managers/userProfileManager.js
@@ -0,0 +1,25 @@
+const _apiUrl = "/api/userprofile";
+
+export const getUserProfiles = () => {
+    return fetch(_apiUrl).then((res) => res.json());
+};
+
+export const getUserProfilesWithRoles = () => {
+    return fetch(_apiUrl + "/withroles").then((res) => res.json());
+};
+
+export const getUserById = (userId) => {
+    return fetch(`${_apiUrl}/${userId}`).then((res) => res.json());
+};
+
+export const promoteUser = (userId) => {
+    return fetch(`${_apiUrl}/promote/${userId}`, {
+        method: "POST",
+    });
+};
+
+export const demoteUser = (userId) => {
+    return fetch(`${_apiUrl}/demote/${userId}`, {
+        method: "POST",
+    });
+};
