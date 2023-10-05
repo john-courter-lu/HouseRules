ffe09e5ec157c5ca00a4cf74779ae18d8492e9ba
John Lu <johnlu2006@gmail.com>
2023-10-05 10:21:09 -0500

    get chores by Id with strictly selective return information

--- Controllers/ChoreController.cs
+++ Controllers/ChoreController.cs
@@ -18,9 +18,43 @@ public class ChoreController : ControllerBase
     }
 
     [HttpGet]  // route /api/chore
-    [Authorize]
+    //[Authorize]
     public IActionResult Get()
     {
         return Ok(_dbContext.Chores.ToList());
     }
+
+    [HttpGet("{id}")] // route /api/chore/{id}
+    //[Authorize] 
+    public IActionResult GetChoreById(int id)
+    {
+        Chore chore = _dbContext.Chores
+        .Include(c => c.ChoreAssignments)
+            .ThenInclude(ca => ca.UserProfile)
+        .Include(c => c.ChoreCompletions)
+
+        .SingleOrDefault(c => c.Id == id);
+
+        if (chore == null)
+        {
+            return NotFound();
+        }
+
+        // Remove UserProfile information from ChoreCompletions
+        // based on the requirement
+        foreach (var completion in chore.ChoreCompletions)
+        {
+            completion.UserProfile = null;
+        }
+
+        // Remove ChoreCompletions information from its current assignees' UserProfile 
+        // to avoid unnecessarily long JSON returns; and unnecessary information
+        foreach (var assignment in chore.ChoreAssignments)
+        {
+            assignment.UserProfile.ChoreCompletions = null;
+        }
+
+        return Ok(chore);
+    }
 }
+
