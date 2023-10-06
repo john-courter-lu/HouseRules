8eafd459abbd8bba6cdf3cb5ef4f5f827bd137b9
John Lu <johnlu2006@gmail.com>
2023-10-06 11:22:02 -0500

    UI component

--- client/src/components/chores/ChoresList.js
+++ client/src/components/chores/ChoresList.js
@@ -1,1 +1,68 @@
-export const ChoresList = ({ loggedInUser }) => {}
+import { useEffect, useState } from "react";
+import { Button, Table } from "reactstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { getChores } from "../../managers/choreManger.js";
+
+export const ChoresList = ({ loggedInUser }) => {
+    const [chores, setChores] = useState([]);
+
+    const navigate = useNavigate();
+
+    const handleDelete = (id) => {
+
+
+    }
+
+    useEffect(() => {
+        getChores().then(setChores);
+    }, [])
+
+
+
+    return (
+        <div className="container">
+            <h4 className="sub-menu">Chores</h4>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Difficulty</th>
+                        <th>Frequency</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {chores.map((c) => (
+                        <tr key={`chores-${c.id}`}>
+                            <th scope="row">{c.id}</th>
+
+                            <td>{c.name}</td>
+
+                            <td>{c.difficulty}</td>
+                            <td>{c.choreFrequencyDays} Day(s)</td>
+
+                            {loggedInUser?.roles.includes("Admin") && (
+                                <>
+                                    <td>
+                                        <Button
+                                            color="danger"
+                                            onClick={() => {
+                                                handleDelete(c.id);
+                                            }}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </td>
+                                </>
+                            )}
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+        </div>
+    )
+}
