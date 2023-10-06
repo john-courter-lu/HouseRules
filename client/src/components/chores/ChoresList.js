c4b3499edf061962e42afcb128fa529968989748
John Lu <johnlu2006@gmail.com>
2023-10-06 15:43:27 -0500

    add a toast message to the onClick complete function

--- client/src/components/chores/ChoresList.js
+++ client/src/components/chores/ChoresList.js
@@ -1,33 +1,42 @@
 import { useEffect, useState } from "react";
-import { Button, Table } from "reactstrap";
+import { Button, Table, Toast, ToastBody, ToastHeader } from "reactstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { completeChore, deleteChore, getChores } from "../../managers/choreManager.js";
 
 export const ChoresList = ({ loggedInUser }) => {
     const [chores, setChores] = useState([]);
 
+    const [toastOpen, setToastOpen] = useState(false);
+    const [toastMessage, setToastMessage] = useState("");
+
     const navigate = useNavigate();
 
     useEffect(() => {
         getChores().then(setChores);
-    }, [])
+    }, []);
 
     const handleDelete = (id) => {
         deleteChore(id).then(() => {
             navigate("/chores");
-        })
-    }
+        });
+    };
 
-    const handleComplete = (id, userId) => {
+    const handleComplete = (id, userId, choreName) => {
         completeChore(id, userId).then(() => {
-            navigate("/chores");
-        })
-    }
+            setToastMessage(`Yay! ${choreName} completed by ${loggedInUser.firstName}!`);
+            setToastOpen(true);
+            // Automatically close the toast after 5 seconds
+            setTimeout(() => {
+                setToastOpen(false);
+            }, 5000);
+
+        });
+    };
 
     return (
         <div className="container">
             <div className="sub-menu">
-                <h4 >Chores</h4>
+                <h4 style={{ margin: "0" }} >Chores</h4>
                 {loggedInUser.roles.includes("Admin") && (
                     <Link to="/chores/create"> <Button>New Chore</Button></Link>
                 )}
@@ -57,7 +66,7 @@ export const ChoresList = ({ loggedInUser }) => {
                                 <Button
                                     color="success"
                                     onClick={() => {
-                                        handleComplete(c.id, loggedInUser.id);
+                                        handleComplete(c.id, loggedInUser.id, c.name);
                                     }}
                                 >
                                     Complete
@@ -89,6 +98,11 @@ export const ChoresList = ({ loggedInUser }) => {
                     ))}
                 </tbody>
             </Table>
+
+            <Toast isOpen={toastOpen} className="position-fixed" style={{ top: "60px", left: "10px" }}>
+                <ToastHeader icon="success">Success</ToastHeader>
+                <ToastBody>{toastMessage}</ToastBody>
+            </Toast>
 
         </div>
     )
